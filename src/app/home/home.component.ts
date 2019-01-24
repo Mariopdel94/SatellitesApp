60acd2cfc057408bf1b370c322914eaa7b09ef02
mario <mariopdel94@gmail.com>
2019-01-23 20:29:02 -0700

    Minor final changes.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -278,10 +278,6 @@ export class HomeComponent implements OnInit {
     return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
   }
 
-  public onSelectChange() {
-    this.getSatellites();
-  }
-
   public downloadResponse() {
     this._satelliteDataService.downloadFile();
   }
